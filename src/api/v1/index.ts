40a081b0501ff0896c9a556a211e43d4c75e8f01
valentinalaura378-cyber <valeninalaura378@gmail.com>
2026-05-11 17:49:36 -0500

    cambios

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,29 +1,21 @@
 import { Router } from "express";
+
 import UserRouter from "../../modules/users/users.routes";
 import AuthRouter from "../../modules/auth/auth.routes";
-<<<<<<< HEAD
 import MakeupRouter from "../../modules/makeup/makeup.routes";
-import BrandRouter from  "../../modules/brand/brand.routes";
-import CategoryRouter from "../../modules/category/category.routes"
+import BrandRouter from "../../modules/brand/brand.routes";
+import CategoryRouter from "../../modules/category/category.routes";
 import InventoryRoutes from "../../modules/inventory/inventory.routes";
-import ReviewsRoutes from "../../modules/reviews/review.routes"
+import ReviewRoutes from "../../modules/reviews/review.routes";
 
- 
-=======
-import MakeupRouter from "../../modules/makeup/makeup.routes"; 
-
->>>>>>> 83861f7f3ba09c729eac251c6940e6726d5f428f
 const router = Router();
 
-router.use('/auth', AuthRouter);
-router.use('/users', UserRouter);
-router.use('/makeup', MakeupRouter); 
-<<<<<<< HEAD
-router.use('/brand', BrandRouter);
-router.use('/category', CategoryRouter);
-router.use('/inventory', InventoryRoutes);
-router.use('/review', ReviewsRoutes);
-=======
->>>>>>> 83861f7f3ba09c729eac251c6940e6726d5f428f
+router.use("/auth", AuthRouter);
+router.use("/users", UserRouter);
+router.use("/makeup", MakeupRouter);
+router.use("/brands", BrandRouter);
+router.use("/categories", CategoryRouter);
+router.use("/inventory", InventoryRoutes);
+router.use("/review", ReviewRoutes);
 
 export default router;
